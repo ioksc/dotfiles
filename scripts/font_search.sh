4df7db87d1e9d7bb0b637faae3bf0b9d6884d8a2
ioksc <ioksc@proton.me>
2024-12-27 21:34:45 -0400

    refactor(font_search): simplify font search script by using parameter expansion
    - Removed usage check for search term and replaced it with parameter expansion for cleaner code.
    - Streamlined the font search command to directly utilize the search term variable.

--- scripts/font_search.sh
+++ scripts/font_search.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <search_term>"
-    exit 1
-fi
-
-# Search for fonts containing the search term and list unique matches
-fc-list | grep -ioE ": [^:]*$1[^:]*" | sed -E 's/(^: |:)//g' | tr ',' '\n' | sort -u
+search_term="${1:-}"
+fc-list | grep -ioE ": [^:]*$search_term[^:]*" | sed -E 's/(^: |:)//g' | tr ',' '\n' | sort -u
