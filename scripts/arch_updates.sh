eb6b3a264368443314e3b09ee76ca65d1f5f8f53
ioksc <ioksc@proton.me>
2024-12-30 20:28:17 -0400

    refactor(arch_updates): restructure update retrieval logic for clarity and efficiency
    - Consolidated the update retrieval process into a new function `get_total_updates` for improved readability.
    - Removed redundant code and streamlined the logic for checking official and AUR updates.
    - Enhanced the click behavior to directly retrieve updates when the block button is pressed, improving user interaction.
    - Maintained output formatting for total updates, ensuring consistent display in the script's output.

--- scripts/arch_updates.sh
+++ scripts/arch_updates.sh
@@ -5,7 +5,6 @@ readonly YAY="/usr/bin/yay"
 readonly PARU="/usr/bin/paru"
 readonly WC="/usr/bin/wc"
 
-
 get_updates() {
     local cmd="$1"
     if [ -x "$cmd" ]; then
@@ -16,25 +15,27 @@ get_updates() {
     fi
 }
 
-if [[ "${BLOCK_BUTTON}" -eq 1 ]]; then
-    exec "$(readlink -f "$0")"
-fi
+get_total_updates() {
+    local official_updates=$(get_updates "$CHECKUPDATES")
+    local aur_updates=0
+    
+    if [ -x "$YAY" ]; then
+        aur_updates=$(get_updates "$YAY" -Qua)
+    elif [ -x "$PARU" ]; then
+        aur_updates=$(get_updates "$PARU" -Qua)
+    fi
+    
+    echo $((official_updates + aur_updates))
+}
 
-official_updates=$(get_updates "$CHECKUPDATES")
-
-aur_updates=0
-if [ -x "$YAY" ]; then
-    aur_updates=$(get_updates "$YAY" -Qua)
-elif [ -x "$PARU" ]; then
-    aur_updates=$(get_updates "$PARU" -Qua)
-fi
-
-total_updates=$((official_updates + aur_updates))
-
-if [ "$total_updates" -gt 0 ]; then
-    echo "<span color='#FF6B6B'>$total_updates</span>"
+# Obtener actualizaciones al hacer click
+if [[ "$BLOCK_BUTTON" -eq 1 ]]; then
+    get_total_updates
 else
-    echo "$total_updates"
+    total_updates=$(get_total_updates)
+    if [ "$total_updates" -gt 0 ]; then
+        echo "<span color='#FF6B6B'>$total_updates</span>"
+    else
+        echo "$total_updates"
+    fi
 fi
-
-exit 0
