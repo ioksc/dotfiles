0f5101606d70f40d25369f83d81e89b7346918ae
ioksc <ioksc@proton.me>
2025-08-10 07:57:34 -0400

    refactor(change_device_output.sh): mejorar la legibilidad del script ajustando la indentación y simplificando la lógica de control

--- scripts/change_device_output.sh
+++ scripts/change_device_output.sh
@@ -2,20 +2,19 @@
 
 # Function to change the audio output
 change_audio_output() {
-    local sink
-    sink=$(pactl list sinks | awk -F ': ' '/Name:/ {print $2}' | fzf --height 40% --reverse --preview "echo {}" --preview-window=up:1)
+	local sink
+	sink=$(pactl list sinks | awk -F ': ' '/Name:/ {print $2}' | fzf --height 40% --reverse --preview "echo {}" --preview-window=up:1)
 
-    if [ -z "$sink" ]; then
-        echo "No sink selected. Exiting."
-        exit 1
-    fi
+	if [ -z "$sink" ]; then
+		echo "No sink selected. Exiting."
+		exit 1
+	fi
 
-    pactl set-default-sink "$sink"
-    if [ $? -eq 0 ]; then
-        echo "Audio output changed to: $sink"
-    else
-        echo "Error changing the audio output."
-    fi
+	if pactl set-default-sink "$sink"; then
+		echo "Audio output changed to: $sink"
+	else
+		echo "Error changing the audio output."
+	fi
 }
 
 # Initial message
